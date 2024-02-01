d95d0f3859fa3a94fbf13827279aa287bebd8cdd
if <if17825578404@126.com>
2024-02-01 22:46:00 +0800

    feat：允许跨域

--- src/main/java/com/portal/config/WebConfig.java
+++ src/main/java/com/portal/config/WebConfig.java
@@ -1,10 +1,10 @@
 package com.portal.config;
 
-import com.portal.interceptors.LoginInterceptor;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import com.portal.interceptors.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +14,7 @@ import org.springframework.http.converter.ResourceHttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.converter.support.AllEncompassingFormHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -41,6 +42,7 @@ public class WebConfig implements WebMvcConfigurer {
         converters.add(new StringHttpMessageConverter());
         converters.add(jackson2HttpMessageConverter());
     }
+
     @Bean
     public MappingJackson2HttpMessageConverter jackson2HttpMessageConverter() {
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
@@ -57,5 +59,14 @@ public class WebConfig implements WebMvcConfigurer {
 
         converter.setObjectMapper(mapper);
         return converter;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH")
+                .maxAge(3600);
     }
 }
