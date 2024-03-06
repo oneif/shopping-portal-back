ee903a4e895ccd80ef58642f173721a01947d616
if <if17825578404@126.com>
2024-03-06 17:26:13 +0800

    feat：解决跨域

---
+++ src/main/java/com/portal/config/CORSFilter.java
@@ -0,0 +1,31 @@
+package com.portal.config;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+
+@Component
+public class CORSFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, HEAD,PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "access-control-allow-origin, authority, content-type, version-info, X-Requested-With,Authorization");
+        HttpServletRequest request = (HttpServletRequest) req;
+        if ("OPTIONS".equals(request.getMethod())) {
+            response.setStatus(HttpStatus.OK.value());
+            response.getWriter().write("OPTIONS returns OK");
+            return;
+        }
+
+        chain.doFilter(req, res);
+    }
+}
--- src/main/java/com/portal/config/WebConfig.java
+++ src/main/java/com/portal/config/WebConfig.java
@@ -14,7 +14,6 @@ import org.springframework.http.converter.ResourceHttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.http.converter.support.AllEncompassingFormHttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -59,14 +58,5 @@ public class WebConfig implements WebMvcConfigurer {
 
         converter.setObjectMapper(mapper);
         return converter;
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowCredentials(true)
-                .allowedMethods("GET", "POST", "DELETE", "PUT", "PATCH")
-                .maxAge(3600);
     }
 }
