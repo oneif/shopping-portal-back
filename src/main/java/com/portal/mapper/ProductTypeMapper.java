262948cab03c1a6f1f545c25f45f9bed5f0ddc7e
if <if17825578404@126.com>
2024-03-06 21:10:20 +0800

    feat：增加商品数据接口

---
+++ src/main/java/com/portal/mapper/ProductTypeMapper.java
@@ -0,0 +1,13 @@
+package com.portal.mapper;
+
+import com.github.yulichang.base.MPJBaseMapper;
+import com.portal.pojo.ProductType;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 操作商品分类相关数据接口
+ */
+@Mapper
+public interface ProductTypeMapper extends MPJBaseMapper<ProductType> {
+
+}
